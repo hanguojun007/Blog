9171e7e0e10899bb769b072e1fe20dc0bde34eed
hanguojun <2867965897@qq.com>
2025-06-22 06:21:41 +0000

    20250622

--- docs/site_libs/gio-binding-0.1.0/gio.js
+++ docs/site_libs/gio-binding-0.1.0/gio.js
@@ -4,7 +4,7 @@ HTMLWidgets.widget({
 
   type: 'output',
 
-  factory: function(el, width, height) {
+  factory: function (el, width, height) {
 
     // TODO: define shared variables for this instance
     var controller;
@@ -12,33 +12,33 @@ HTMLWidgets.widget({
 
     return {
 
-      renderValue: function(x) {
+      renderValue: function (x) {
 
         // TODO: code to render the widget, e.g.
         // el.innerText = x.message;
         // var container = document.getElementById("globe");
         // long to wide
         // x.data = HTMLWidgets.dataframeToD3(x.data);
-        controller = new GIO.Controller(el);
+        controller = new GIO.Controller(el, x.configs);
         controller.addData(x.data);
         controller.setStyle(x.style); // set style
 
         // gio.js
-        if(x.stats)
+        if (x.stats)
           controller.enableStats();
 
         controller.init();
 
         sel_handle.setGroup(x.crosstalk.group);
 
-        function callback (selectedCountry) {
+        function callback(selectedCountry) {
           sel_handle.set([selectedCountry.ISOCode]);
         }
 
         controller.onCountryPicked(callback);
 
         // placed in factory function
-        sel_handle.on("change", function(e) {
+        sel_handle.on("change", function (e) {
 
           // selection comes from another widget
           if (e.sender !== sel_handle) {
@@ -50,7 +50,7 @@ HTMLWidgets.widget({
 
       },
 
-      resize: function(width, height) {
+      resize: function (width, height) {
 
         // TODO: code to re-render the widget with a new size
         controller.resizeUpdate();
